747844f03628fa2daba39314d8966665754261ef
nash15963 <nash1596315963@gmail.com>
2022-03-11 11:40:24 +0800

    2022/03/11 2

--- static/index.js
+++ static/index.js
@@ -1,91 +1,94 @@
-let Enroll =()=> {
-  
-  let content = document.querySelector('.content');
-  let Body = document.querySelector('body');
-  const getDatas = async (page) => {
+let input = '';  //預設沒有輸入關鍵字查找
+let page = 0; //第一頁預設0
+let content = document.querySelector('.content');
+let btn = document.querySelector('#btn');
+var nextPage = 0 ;
 
-
-      const API_URL = `/api/attractions?page=${page}`;
-      const response = await fetch(API_URL);
-      // handle 404
-      if (!response.ok) {
-          throw new Error(`An error occurred: ${response.status}`);
-      }
-      return await response.json();
-  }
-
-  const showDatas = (dataList) => {
-      dataList.forEach(data => {
-          const boxEle = document.createElement('div');
-          boxEle.classList.add('box');
-
-          boxEle.innerHTML = `
-          <div>
-              <img src="${data.img[0]}" >
-          </div>
-          
-          <p>${data.name}</p>
-          <p>${data.mrt}</p>
-          <p>${data.category}</p>
-      `;
-
-          content.appendChild(boxEle);
-      });
-  };
-  
-  const hasMoreDatas = (e) => {
-      return e !== null ;
-  };
-  
-  const loadDatas = async (page) => {
-
-      // // show the loader
-      
-
-      // 0.5 second later
-      setTimeout(async () => {
-          try {
-              // if having more quotes to fetch
-              if (hasMoreDatas(nextPage)) {
-                  // call the API to get quotes
-                  const response = await getDatas(page);
-                  // show quotes
-                  showDatas(response.data);
-                  // update the total
-                  nextPage = response.nextPage;
-              }
-          } catch (error) {
-              console.log('error.message',error.message);
-          }
-      }, 1000);
-
-  };
-  let currentPage = 0;
-  let nextPage = 0;
-  
-  window.addEventListener('scroll', () => {
-      const {
-          scrollTop,
-          scrollHeight,
-          clientHeight
-      } = document.documentElement;
-
-      if (scrollTop + clientHeight >= scrollHeight - 4 &&
-          hasMoreDatas(currentPage)) {
-          currentPage++;
-          loadDatas(currentPage);
-      }
-     
-  }, {
-      passive: true
-  });
-   // initialize
-   loadDatas(currentPage);
-
+//取得fetch資料
+const getDatas = async (page) => {
+    if (input != '') {
+        API_URL = `/api/attractions?page=${page}&keyword=${input}`;
+    } else {
+        API_URL = `/api/attractions?page=${page}`;
+    }
+    const response = await fetch(API_URL);
+    //console.log(API_URL)
+    // handle 404
+    if (!response.ok) {
+        throw new Error(`An error occurred: ${response.status}`);
+    }
+    return await response.json();
+}
+//插入元素box，裡面置入圖片存放點
+const showDatas = (dataList) => {
+    dataList.forEach(data => {
+        const boxEle = document.createElement('div');
+        boxEle.classList.add('box');
+        boxEle.innerHTML = `
+        <div>
+            <img src="${data.img[0]}" >
+        </div>
+        
+        <p>${data.name}</p>
+        <p>${data.mrt}</p>
+        <p>${data.category}</p>
+    `;
+        content.appendChild(boxEle);
+    });
 };
-Enroll()
+//過濾字串
+let queryKeyword = (e) => {
+    e.preventDefault();
+    input = document.querySelector('#keyWord').value;
+    content.innerHTML = '';
+    loadDatas(page, input) ; //過濾完字串則抓取api，空白字串則進入沒有keyword路徑
+};
+btn.addEventListener('click', queryKeyword);
 
-
-
-
-
+// search api
+const hasMoreDatas = (e) => {
+    console.log('nextPage :',e)
+    return e !== null;
+};
+const loadDatas = async (page, input) => {
+    // // show the loader(keep it)
+    // 0.5 second later
+    setTimeout(async () => {
+        try {
+            // if having more data to fetch
+            if (hasMoreDatas(nextPage)) {
+                // call the API to get data
+                const response = await getDatas(page, input);
+                // show data
+                showDatas(response.data);
+                // update the total
+                nextPage = response.nextPage;
+                // console.log('input:', input); 除錯輸入字串
+                
+            }
+        } catch (error) {
+            const errorMessage = document.createElement('h3')
+            errorMessage.innerText = '找不到您所要的資料'
+            errorMessage.style.color = '#666666'
+            content.append(errorMessage)
+        }
+    }, 1000);
+    //可以嘗試增加讀取動畫(不然看起來很像當機)
+};
+let currentPage = 0;
+window.addEventListener('scroll', () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+    //滑到底部則讀取api資料
+    if (scrollTop >= scrollHeight - clientHeight && hasMoreDatas(nextPage)) {
+        currentPage++;
+        loadDatas(currentPage);
+    }
+}, {
+    passive: true
+});
+// initialize
+loadDatas(currentPage, input);
