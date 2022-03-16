7e8955ef1846abbb1f937b4a0733005b42cc7c38
nash15963 <nash1596315963@gmail.com>
2022-03-17 02:04:58 +0800

    20220317_I_want_to_kill_this_page

--- static/attraction.js
+++ static/attraction.js
@@ -1,8 +1,10 @@
 //擷取網址ID到fetch
 const topContent = document.querySelector('.top-content');
 let UrlId = window.location.pathname.split('/')[2]
-console.log(UrlId)
-let imgId = 0 //圖片ID位置
+//console.log(UrlId)
+let imgArrayLen = 0 ;//圖片張數
+
+
 //fetch api
 const getDatas = async (id) => {
 	API_URL = `/api/attraction/${id}`;
@@ -13,38 +15,19 @@ const getDatas = async (id) => {
 	} //若資料錯誤回傳結果
 	return await response.json(); //向global丟出得到的api資料
 }
-//將資料填入網頁裡
+
+//li的父代
+cir = document.querySelector('.cir')
+//介紹資料渲染網頁
 const showDatas = (response) => {
-	console.log('data :', response.data)
-	let topImg = document.querySelector('.top-img')
-	let Img = document.createElement('img');
+	//console.log('data1 :', response.data)
 	imgArrayLen = response.data.img.length;
-	Img.src = response.data.img[imgId]
-	Img.classList.add('slideshadow')
-	topImg.appendChild(Img)
-	let shadow = document.querySelector('.slideshadow')
-	//左右點擊切換圖片
-	let leftArrow = document.querySelector('#leftArrow')
-	let rightArrow = document.querySelector('#rightArrow')
-	let = nextImg = () => {
-		if (imgId < imgArrayLen - 1) {
-			imgId++
-			console.log(imgId)
-			shadow.removeAttribute('src')
-			Img.src = response.data.img[imgId]
-		}
+	//渲染li(有幾張圖則掛幾個白點上去)
+	for(let i=0 ;i<imgArrayLen ;i++){
+		let li = document.createElement('li');
+		cir.appendChild(li) ;
 	}
-	rightArrow.addEventListener('click', nextImg)
-	previousImg = () => {
-		if (imgId > 0) {
-			imgId--
-			console.log(imgId)
-			shadow.removeAttribute('src')
-			Img.src = response.data.img[imgId]
-		}
-	}
-	leftArrow.addEventListener('click', previousImg)
-	// ### 是否要移出來呢###
+
 	let topIntroduction = document.querySelector('.top-introduction')
 	topIntroduction.innerHTML = `
         <h3>${response.data.name}</h3>
@@ -61,10 +44,69 @@ const showDatas = (response) => {
         <p>${response.data.transport}</p>
     `
 };
+
+let imgId = 0 //圖片ID位置
+
+let topImg = document.querySelector('.top-img') //圖片父代
+let leftArrow = document.querySelector('#leftArrow') //左箭頭元素
+let rightArrow = document.querySelector('#rightArrow') //右箭頭元素
+
+//左右點擊置換圖片，加上索引
+const shadowImg = (response)=>{
+	//console.log('data2 :', response.data.img[imgId]) 
+	let Img = document.createElement('img');
+	Img.src = response.data.img[imgId]  //掛上第一張圖
+	Img.classList.add('slideshadow')
+	topImg.appendChild(Img)
+	let shadow = document.querySelector('.slideshadow') //準備被置換的圖片
+	//點點的部分
+	let focus_point = document.querySelector(`.cir li:nth-child(${imgId+1})`) //抓取元素li
+	focus_point.classList.add('cir_focus')  //在第一個點掛上黑色 
+	let blackPoint = document.querySelector('.cir_focus')//準備被置換的黑點
+	// console.log(imgArrayLen)
+	// let leftMovePoint = 200-imgArrayLen*6 ;
+	// console.log(leftMovePoint)
+	// cir.style = `left:${leftMovePoint}px`
+	//左右點擊切換圖片
+	
+	const addFocusFlip =()=>{
+		blackPoint = document.querySelector('.cir_focus')//準備被置換的黑點
+		blackPoint.removeAttribute('class') //移除黑點(class)
+		focus_point = document.querySelector(`.cir li:nth-child(${imgId+1})`) //選擇下一個元素
+		focus_point.classList.add('cir_focus') //增加黑點
+	}
+
+	let nextImg = () => {
+		if (imgId < imgArrayLen - 1) {
+			imgId++
+			//console.log(imgId)
+			shadow.removeAttribute('src')
+			Img.src = response.data.img[imgId] //換圖片讀取
+			addFocusFlip() //用函數替換
+
+		}
+	}
+	rightArrow.addEventListener('click', nextImg)
+	
+	let previousImg = () => {
+		if (imgId > 0) {
+			imgId--
+			//console.log(imgId)
+			shadow.removeAttribute('src')
+			Img.src = response.data.img[imgId]
+			addFocusFlip()
+		}
+	}
+	leftArrow.addEventListener('click', previousImg)
+	
+}
+
+//進入頁面則渲染畫面
 const firstRender = async (id) => {
 	try {
 		response = await getDatas(id)
 		showDatas(response)
+		shadowImg(response)
 	} catch {
 		topContent.innerHTML = '';
 		const errorMessage = document.createElement('h3')
@@ -74,6 +116,8 @@ const firstRender = async (id) => {
 	}
 }
 firstRender(UrlId)
+
+
 //改變金額
 let fullday_moring = document.querySelector('.fullday_moring')
 let fullday_evening = document.querySelector('.fullday_evening')
@@ -87,4 +131,3 @@ if (document.querySelector('input[name="fullday"]')) {
 		});
 	});
 }
-//插入預覽圖片 ??
