61b77ae17e53ef6603fda5785f510dd2da391bca
nash15963 <nash1596315963@gmail.com>
2022-03-18 16:46:13 +0800

    add note in Pool 0318

--- xxx.py
+++ xxx.py
@@ -10,23 +10,37 @@ import pymysql
 import os
 from dotenv import load_dotenv #python-dotenv
 import json
-
-
+from dbutils.pooled_db import PooledDB
 load_dotenv()
-connection  = pymysql.connect(host='127.0.0.1',
-                              user='root',
-                              password= os.getenv('self_mysql_PW'),
-                              database='taipeitrip',
-                              charset='utf8',
-                              cursorclass=pymysql.cursors.DictCursor)
-cursor = connection.cursor()
+
+POOL = PooledDB(
+    creator=pymysql,  # 負責連結的python模組
+    maxconnections=6,  # 連接池的最大連接數，0和None表示不限制連接次數
+    mincached=2,  # 初始化时，連接池中至少創建的個數
+    maxcached=5,  # 連接池中最大的連接個數
+    maxshared=3,  # 連接池中最多共享的連接数量。
+    blocking=True,  # 連接池中如果没有可用連接後，進行等待:True，等待；False，不等待然後error
+    maxusage=None,  # 一個鍊結最多被重複使用的次數
+    setsession=[],  # 開始會話前的命令列。如：["set datestyle to ...", "set time zone ..."]
+    ping=0, #自己看文件(好麻煩)
+    host='127.0.0.1',
+    user='root',
+    password= os.getenv('self_mysql_PW'),
+    database='taipeitrip',
+    charset='utf8',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 
 @app.route("/api/attraction/<attractionId>")
 def attractionId(attractionId):
     print('id : ',attractionId)
+    conn = POOL.connection()
+    cursor = conn.cursor()
     sql = "SELECT id,name,category,description,address,transport,mrt,latitude,longitude,img FROM location where id = %s; "
     sql_run = cursor.execute(sql,(attractionId))
-    result = cursor.fetchone()          
+    result = cursor.fetchone() 
+    conn.close()         
     if sql_run == 1:
         result['img'] = result['img'].split(',')
         result_JSON = json.dumps({"data":result},ensure_ascii=False)	    
@@ -42,6 +56,8 @@ def api_attraction():
     keyword = args.get("keyword", default="", type=str)
     now_page = int(page)*12
     now_keyword = '%'+keyword+'%'
+    conn = POOL.connection()
+    cursor = conn.cursor()
     if keyword == '' :
         sql = "SELECT id,name,category,description,address,transport,mrt,latitude,longitude,img FROM location limit %s,12;"
         sql_run = cursor.execute(sql,now_page) 
@@ -49,6 +65,7 @@ def api_attraction():
         sql = "SELECT id,name,category,description,address,transport,mrt,latitude,longitude,img FROM location where name like %s limit %s,12;"
         sql_run = cursor.execute(sql,(now_keyword,now_page))
     result = cursor.fetchall()
+    conn.close()
     if sql_run < 12 :
         page_now = None
     else :
@@ -88,7 +105,7 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-app.run(port=3000)
+app.run(port=3000,debug=True)
 
 #http://127.0.0.1:3000/api/attraction/1
 #http://127.0.0.1:3000/api/attractions?page=0
